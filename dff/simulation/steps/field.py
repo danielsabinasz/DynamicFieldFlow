6b53de36a0a82be1ade45599547395c9e0b20a6e
Daniel Sabinasz <daniel.sabinasz@ruhr-uni-bochum.de>
2022-08-20 12:36:01 +0200

    Set some TF variable names for optimization of field and kernel parameters

--- dff/simulation/steps/field.py
+++ dff/simulation/steps/field.py
@@ -20,10 +20,10 @@ def field_prepare_constants(step):
 
 
 def field_prepare_variables(step):
-    resting_level = tf.Variable(step.resting_level)
+    resting_level = tf.Variable(step.resting_level, name=step.name + ".resting_level")
     time_scale = tf.Variable(step.time_scale)
     sigmoid_beta = tf.Variable(step.activation_function.beta)
-    global_inhibition = tf.Variable(step.global_inhibition)
+    global_inhibition = tf.Variable(step.global_inhibition, name=step.name + ".global_inhibition")
     noise_strength = tf.Variable(step.noise_strength)
 
     domain = step.domain()
