Fix projection kernel computation

dff/simulation/simulator_graph.py
@@ -287,7 +287,7 @@ def simulate_time_step(time_step, start_time, time_step_duration, steps, input_s
     return new_values
 
 
-#@tf.function
+@tf.function
 def compute_output(input, activation_function_type, beta):
     logger.debug(f"trace apply_projection {input} {beta}")
 

dff/simulation/steps/field.py
@@ -64,12 +64,12 @@ def field_prepare_time_and_variable_invariant_tensors(shape, domain):
 
 # TODO performance
 #@tf.function
-#def field_compute_time_invariant_variable_variant_tensors(shape, interaction_kernel_positional_grid, resting_level,
-#                                                          interaction_kernel_weight_pattern_config):
-#    resting_level_tensor = tf.ones(tuple([int(x) for x in shape])) * resting_level
-#    interaction_kernel_weight_pattern_tensor = compute_weight_pattern_tensor(interaction_kernel_weight_pattern_config,
-#                                                                              interaction_kernel_positional_grid)
-#    return resting_level_tensor, interaction_kernel_weight_pattern_tensor
+def field_compute_time_invariant_variable_variant_tensors(shape, interaction_kernel_positional_grid, resting_level,
+                                                          interaction_kernel_weight_pattern_config):
+    resting_level_tensor = tf.ones(tuple([int(x) for x in shape])) * resting_level
+    interaction_kernel_weight_pattern_tensor = compute_weight_pattern_tensor(interaction_kernel_weight_pattern_config,
+                                                                              interaction_kernel_positional_grid)
+    return resting_level_tensor, interaction_kernel_weight_pattern_tensor
 
 
 @tf.function