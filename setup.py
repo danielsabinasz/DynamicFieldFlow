600efd4531a024ca28e164efc4e5fd2f435e31e7
Daniel Sabinasz <daniel@sabinasz.net>
2023-04-18 17:01:44 +0200

    Add support for Richard's Mac

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@ from setuptools import setup, find_packages
 import sysconfig
 
 platform = sysconfig.get_platform()
-is_m1_mac = platform.startswith("mac") and "universal" in platform
+is_m1_mac = platform.startswith("mac") and "universal" in platform or platform == "macosx-11.1-arm64"
 
 if is_m1_mac:
     install_requires = [
         'DynamicFieldPy',
         'tensorflow-macos==2.8.0',
+        'matplotlib==3.5.1',
+        'numpy==1.22.3',
+        'tensorflow-probability==0.15.0',
+        'protobuf==3.20.0'
+    ]
+elif platform == "macosx-11.1-arm64" or platform == "linux-x86_64":
+    install_requires = [
+        'DynamicFieldPy',
+        'tensorflow-macos==2.9.0',
         'matplotlib==3.5.1',
         'numpy==1.22.3',
         'tensorflow-probability==0.15.0',
